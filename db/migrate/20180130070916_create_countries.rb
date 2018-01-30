f93e90a5d6ea60a84ff6cebe19cf50f4d0cd0a11
根本大輔 <defmono112233@gmail.com>
2018-01-30 16:24:43 +0900

    countryモデル作成

---
+++ db/migrate/20180130070916_create_countries.rb
@@ -0,0 +1,8 @@
+class CreateCountries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :countries do |t|
+      t.string :name, null: false, unique: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130065810) do
+ActiveRecord::Schema.define(version: 20180130070916) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "comment",    null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20180130065810) do
     t.datetime "updated_at", null: false
     t.index ["review_id"], name: "index_comments_on_review_id", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "countries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
