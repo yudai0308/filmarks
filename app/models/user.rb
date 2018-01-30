7fe981a2fa2a4a4cff7da033c7728522e8a72480
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-30 15:02:39 +0900

    Create_User_model
    Userモデルの作成
    全アソシエーション記載
    Null:falseに対してバリデーションを定義

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :Filmarks_ID, presence: true
+  validates :nickname, presence: true
+        
+
+  has_many :reviews
+  has_many :likes
+  has_many :comments
+  has_many :movies, through: :clips
+  has_many :clips
+  has_many :members, through: :users_members
+  has_many :users_members
+  has_many :active_relationships, class_name:  "relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name:  "relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower       
+end
