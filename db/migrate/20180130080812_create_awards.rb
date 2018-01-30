6256b7198f47a42947f1958a72a148e7c2dba816
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-30 17:57:01 +0900

    Create Movies_Genre_model
    Movies_Genreモデルの生成

--- db/migrate/20180130080812_create_awards.rb
+++ db/migrate/20180130080812_create_awards.rb
@@ -2,7 +2,6 @@ class CreateAwards < ActiveRecord::Migration[5.0]
   def change
     create_table :awards do |t|
       t.string :name, null: false, unique: true
-
       t.timestamps
     end
   end
