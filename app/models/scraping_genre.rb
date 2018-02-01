bdda552b28f5b5eac85293c6494683182853f2b0
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-01 09:25:27 +0900

    Edit Scraping name
    Genreテーブルのスクレイピング名及びメソッド名を変更

--- app/models/scraping_genre.rb
+++ app/models/scraping_genre.rb
@@ -1,5 +1,5 @@
-class Scraping
-  def self.movie_urls
+class Scraping_genre
+  def self.info_genre
     agent = Mechanize.new
     page = agent.get("https://filmarks.com/list/genre")
     elements = page.search('.c-search-panel__main li a')
