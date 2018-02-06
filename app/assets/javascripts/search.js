cffae4a9e37c65935764a77fed221173d6a822d9
根本大輔 <defmono112233@gmail.com>
2018-02-06 16:27:29 +0900

    edit

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,27 @@
+$(function(){
+  $("#q.l-search-field__query").on("keyup",function(){
+    var target = $('#js-search-list')
+    var input = $("#q").val();
+    var html= ` <ul id="search_lists">
+                  <li class="p-header-my-menu__item active">
+                    <button name="button" type="submit" class="search__btn" value="movie">
+                      <span>「${input}」で映画を検索</span>
+                    </button>
+                  </li>
+                  <li class="p-header-my-menu__item">
+                    <button name="button" type="submit" class="search__btn" value="cast">
+                      <span>「${input}」でキャストーを検索</span>
+                    </button>
+                  </li>
+                  <li class="p-header-my-menu__item">
+                    <button name="button" type="submit" class="search__btn" value="user">
+                      <span>「${input}」でユーザーを検索</span>
+                    </button>
+                  </li>
+                </ul>`
+      $('#search_lists').remove();
+    if (input.length !== 0){
+      target.append(html)
+    }
+  });
+});
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,19 @@ class MoviesController < ApplicationController
   end
 
   def search
-    @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
+    if params[:button] == "user"
+# Viewが出来たら変更↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
+      render action:'search_movie'
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    elsif params[:button] == "cast"
+# Viewが出来たら変更↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
+      render action:'search_movie'
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    else
+      @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
+      render action:'search_movie'
+    end
   end
 end
