db074d73912d697d84d78edfb51e7598ad6ecb91
根本大輔 <defmono112233@gmail.com>
2018-02-07 12:46:27 +0900

    [add]search_cast

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,17 +9,21 @@ class MoviesController < ApplicationController
   def search
     if params[:button] == "user"
 # Viewが出来たら変更↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-      @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
-      render action:'search_movie'
+      @users_count = User.where('nickname LIKE(?)', "%#{params[:keyword]}%").count
+      @users = User.where('nickname LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
+      render action:'search_user'
 #↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
     elsif params[:button] == "cast"
 # Viewが出来たら変更↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-      @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
-      render action:'search_movie'
+      @casts_count = Member.where('name LIKE(?)', "%#{params[:keyword]}%").count
+      @casts = Member.where('name LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(24)
+      render action:'search_cast'
 #↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
     else
+      @movies_count = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").count
       @movies = Movie.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(36)
       render action:'search_movie'
     end
+    # binding.pry
   end
 end
