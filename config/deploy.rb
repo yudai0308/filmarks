01337b6964c8b1d71f2dcb397f523c77579307a6
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-09 20:05:00 +0900

    RDS update test
    データベースアップロードテスト

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,16 @@ set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
   task :restart do
     invoke 'unicorn:restart'
   end
