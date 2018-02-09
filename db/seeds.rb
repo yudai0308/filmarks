e03a217bea8f6edd4e5445c0a1494a98802cf360
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-09 20:08:35 +0900

    Edit
    不具合修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "csv"
 orders_csv = CSV.readlines("db/awards.csv")
 orders_csv.shift
 orders_csv.each do |row|
-  Order.create(name: row[1], created_at: row[2], updated_at: row[3], subname: row[4])
+  Award.create(name: row[1], created_at: row[2], updated_at: row[3], subname: row[4])
 end
 
 products_csv = CSV.readlines("db/products.csv")
