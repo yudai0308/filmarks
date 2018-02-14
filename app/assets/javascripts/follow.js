dd97cddc12baba7ffbd66b92a9e8a630d481ae97
根本大輔 <defmono112233@gmail.com>
2018-02-14 11:45:44 +0900

    edit_非同期での書き換えに変更中

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,15 +1,49 @@
 $(function(){
-  $('.user__lists-content-follow__btn.follow-btn').click(function (e) {
+  $(document).on("click",'.unfollow-btn',function(e){
+    e.stopPropagation();
     e.preventDefault();
+    $(this).removeClass('unfollow-btn').addClass('follow-btn')
     console.log('followした')
-    console.log(this)
+    var link = $(this).parent();
+    var url = link.attr('href');
+    var m = link.attr('method');
+    var data = link.data
+    console.log(link)
+    console.log(url)
+    console.log(m)
+    console.log(data)
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      processData: false,
+      contentType: false
+    });
   });
 });
 
 $(function(){
-  $('.user__lists-content-follow__btn.unfollow-btn').click(function (e) {
+  $(document).on("click",'.follow-btn',function(e){
+    e.stopPropagation();
     e.preventDefault();
+    $(this).removeClass('follow-btn').addClass('unfollow-btn')
     console.log('unfollowした');
-    console.log(this)
+    var link = $(this).parent();
+    var url = link.attr('href');
+    var m = link.attr('method');
+    var data = link.data
+    console.log(link)
+    console.log(url)
+    console.log(m)
+    console.log(data)
+
+    $.ajax({
+      url: url,
+      type: "DELETE",
+      processData: false,
+      contentType: false
+    });
+
+
   });
 });
