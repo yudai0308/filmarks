251c6c7ccab2ae041a9dae69eecbdbb14a126f83
根本大輔 <defmono112233@gmail.com>
2018-02-15 00:28:15 +0900

    edit_フォロー非同期通信化

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -2,22 +2,27 @@ $(function(){
   $(document).on("click",'.unfollow-btn',function(e){
     e.stopPropagation();
     e.preventDefault();
-    $(this).removeClass('unfollow-btn').addClass('follow-btn')
-    console.log('followした')
-    var link = $(this).parent();
-    var url = link.attr('href');
-    var m = link.attr('method');
-    var data = link.data
-    console.log(link)
-    console.log(url)
-    console.log(m)
-    console.log(data)
-
+    var btn = $(this);
+    var element = $(this).parent();
+    var url = element.attr('href');
     $.ajax({
       url: url,
       type: "POST",
+      data: "",
+      dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      $(btn).removeClass('unfollow-btn').addClass('follow-btn')
+      $(element).attr('href', `/users/${data.active_user_id}/relationships/${data.passive_user_id}`)
+      $(`#followers-count_userid${data.passive_user_id}`).text(data.passive_count)
+      if ($(`#followings-count_userid${data.active_user_id}`).length){
+        $(`#followings-count_userid${data.active_user_id}`).text(data.active_count)
+      }
+    })
+    .fail(function(){
+      alert('通信に失敗しました。更新ボタンを押してリロードして下さい。');
     });
   });
 });
@@ -26,24 +31,27 @@ $(function(){
   $(document).on("click",'.follow-btn',function(e){
     e.stopPropagation();
     e.preventDefault();
-    $(this).removeClass('follow-btn').addClass('unfollow-btn')
-    console.log('unfollowした');
-    var link = $(this).parent();
-    var url = link.attr('href');
-    var m = link.attr('method');
-    var data = link.data
-    console.log(link)
-    console.log(url)
-    console.log(m)
-    console.log(data)
-
+    var btn = $(this);
+    var element = $(this).parent();
+    var url = element.attr('href');
     $.ajax({
       url: url,
       type: "DELETE",
+      data:"",
+      dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      $(btn).removeClass('follow-btn').addClass('unfollow-btn')
+      $(element).attr('href', `/users/${data.active_user_id}/relationships/?followed_id=${data.passive_user_id}`)
+      $(`#followers-count_userid${data.passive_user_id}`).text(data.passive_count)
+      if ($(`#followings-count_userid${data.active_user_id}`).length){
+        $(`#followings-count_userid${data.active_user_id}`).text(data.active_count)
+      }
+    })
+    .fail(function(){
+      alert('通信に失敗しました。更新ボタンを押してリロードして下さい。');
     });
-
-
   });
 });
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,16 +3,45 @@ class RelationshipsController < ApplicationController
   def create
     user = params[:followed_id]
     current_user.follow(user)
-        binding.pry
-    redirect_to controller: 'lists', action: 'user'
-
+    @passive_user_followers_count = passive_user_followers_count(user)
+    @active_user_followers_count = active_user_followings_count(current_user.id)
+    respond_to do |format|
+      format.html { redirect_to controller: 'lists', action: 'user' }
+      format.json {
+        render json: {
+          passive_count: @passive_user_followers_count,
+          active_count: @active_user_followers_count,
+          active_user_id: current_user.id,
+          passive_user_id: user,
+        }
+      }
+    end
   end
 
   def destroy
     user = params[:id]
     current_user.unfollow(user)
-        binding.pry
-    redirect_to controller: 'lists', action: 'user'
-
+    @passive_user_followers_count = passive_user_followers_count(user)
+    @active_user_followers_count = active_user_followings_count(current_user.id)
+    respond_to do |format|
+      format.html { redirect_to controller: 'lists', action: 'user' }
+      format.json {
+        render json: {
+          passive_count: @passive_user_followers_count,
+          active_count: @active_user_followers_count,
+          active_user_id: current_user.id,
+          passive_user_id: user,
+        }
+      }
+    end
   end
+
+  private
+  def passive_user_followers_count(user_id)
+    User.find_by(id:user_id).followers.count
+  end
+  def active_user_followings_count(user_id)
+    User.find_by(id:user_id).following.count
+  end
+
 end
