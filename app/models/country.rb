56b56d2068cdeb7b5ac8c9965e118850b4a41d6c
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-15 19:55:04 +0900

    Edit

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,6 @@
 class Country < ApplicationRecord
   has_many :movies, through: :movies_countries
-  has_many :movies_countries, foreign_key: 'country_id'
+  has_many :movies_countries, foreign_key: true
 
-  validates :name ,
-            presence: true
+  validates :name, presence: true
 end
