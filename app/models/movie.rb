04c8f37b89bb9d4d4172f80f231d876304c2b713
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-15 19:24:32 +0900

    Add  foreign_key
    foreign_keyの記述を追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,13 +3,13 @@ class Movie < ApplicationRecord
   has_many :users, through: :clips
   has_many :clips, dependent: :destroy
   has_many :members, through: :movies_members
-  has_many :movies_members, dependent: :destroy
+  has_many :movies_members, dependent: :destroy, foreign_key: 'movie_id'
   has_many :countrys, through: :movies_countrys
-  has_many :movies_countrys, dependent: :destroy
+  has_many :movies_countrys, dependent: :destroy, foreign_key: 'movie_id'
   has_many :genres, through: :movies_genres
-  has_many :movies_genres, dependent: :destroy
+  has_many :movies_genres, dependent: :destroy, foreign_key: 'movie_id'
   has_many :awards, through: :movies_awards
-  has_many :movies_awards, dependent: :destroy
+  has_many :movies_awards, dependent: :destroy, foreign_key: 'movie_id'
 
   validates :title, presence: true
 end
