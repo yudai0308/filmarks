66ca295a35d62caf1758eb4fcf0e62819bead20b
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-16 10:48:43 +0900

    Edit models

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,8 @@
 class Member < ApplicationRecord
   has_many :movies, through: :movies_members
-  has_many :movies_members, dependent: :destroy, foreign_key: true
+  has_many :movies_members, dependent: :destroy
   has_many :users, through: :users_members
   has_many :users_members, dependent: :destroy
   validates :name, presence: true
   validates :status, presence: true
-
-  accepts_nested_attributes_for :movies_members, allow_destroy: true
 end
