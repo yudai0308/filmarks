524df7bf71896c506753041cdad5998548b81702
根本大輔 <defmono112233@gmail.com>
2018-02-17 18:14:09 +0900

    [fix]lists_controllerとindex&yearビュー修正

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,7 +25,7 @@ class ListsController < ApplicationController
   end
 
   def year
-    @movies_age = Movie.group(:time).order('time DESC')
+    @movies_age = Movie.group(:production).order('production DESC').map{|movie| movie.production.to_i}
   end
 
   def tag
@@ -36,15 +36,15 @@ class ListsController < ApplicationController
   end
 
   def index
-    @movies_now = Movie.order('time DESC').limit(10).order("RAND()").limit(5)
-    @movies_comming  = Movie.order('time DESC').limit(5)
-    @movies_upcomming  = Movie.order('time DESC').limit(5)
+    @movies_now = Movie.order('production DESC').limit(10).order("RAND()").limit(5)
+    @movies_comming  = Movie.order('production DESC').limit(5)
+    @movies_upcomming  = Movie.order('production DESC').limit(5)
     @movies_trend  = Movie.order("RAND()").limit(5)
     @countries = Country.all
     @genres = Genre.all
     @awards = Award.group(:name)
-    @movies_age = Movie.group(:time).order('time DESC')
-    @movies_rental = Movie.order('time DESC').limit(10).order("RAND()").limit(3)
+    @movies_age = Movie.group(:production).order('production DESC').map{|movie| movie.production.to_i}
+    @movies_rental = Movie.order('production DESC').limit(10).order("RAND()").limit(3)
   end
   def user
     # ↓フォロワー数で取得？
