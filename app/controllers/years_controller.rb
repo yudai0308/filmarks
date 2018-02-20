2472e0e081c225170c8982462a5571610ac749bf
根本大輔 <defmono112233@gmail.com>
2018-02-20 10:05:54 +0900

    edit_yearscontroller

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,2 +1,12 @@
 class YearsController < ApplicationController
+  def index
+    movies = Movie.order('production DESC')
+    @movies = movies.group(:production).map{|movie| [movie.production.to_i, Movie.where(production: movie.production)]}.to_h
+  end
+
+
+  def show
+    movies = Movie.order('production DESC')
+    @movies = movies.group(:production).map{|movie| [movie.production.to_i, Movie.where(production: movie.production)]}.to_h
+  end
 end
