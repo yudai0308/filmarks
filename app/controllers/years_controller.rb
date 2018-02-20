07bbade123b6777383d5549cee86ca70934e6dd2
根本大輔 <defmono112233@gmail.com>
2018-02-20 13:44:12 +0900

    [add]ビューに映画表示

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,12 +1,26 @@
 class YearsController < ApplicationController
+
   def index
-    movies = Movie.order('production DESC')
-    @movies = movies.group(:production).map{|movie| [movie.production.to_i, Movie.where(production: movie.production)]}.to_h
+    @search_age_name = params[:age_id].to_i
+    first_year = @search_age_name
+    last_year = @search_age_name +10
+    movies_all = Movie.order('production DESC')
+    @search_movies =  movies_all.where("production >= ?",first_year).where("production < ?",last_year)
+    @search_movies_count = @search_movies.count
+    @page_of_movie = @search_movies.page(params[:page]).per(36)
+    @movies_age = movies_all.group(:production).map{|movie| movie.production.to_i}
   end
 
 
   def show
-    movies = Movie.order('production DESC')
-    @movies = movies.group(:production).map{|movie| [movie.production.to_i, Movie.where(production: movie.production)]}.to_h
+    @search_age_name = params[:age_id].to_i
+    @search_year_name = params[:id].to_i
+    movies_all = Movie.order('production DESC')
+    @search_movies =  movies_all.where("production = ?", @search_year_name)
+    @search_movies_count = @search_movies.count
+    @page_of_movie = @search_movies.page(params[:page]).per(36)
+    @movies_age = movies_all.group(:production).map{|movie| movie.production.to_i}
+    render 'index'
   end
+
 end
