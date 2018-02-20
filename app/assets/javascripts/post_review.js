99c3f0a54bef54d0260c70a9383c560d0913b7dc
Yudai Watanabe <lock0308@gmail.com>
2018-02-21 01:48:10 +0900

    映画一覧ページからのレビュー投稿機能を実装

--- app/assets/javascripts/post_review.js
+++ app/assets/javascripts/post_review.js
@@ -1,46 +1,20 @@
 $(function() {
-  function buildReviewHTML(reviewURL) {
-
-  var reviewHTML =
-     `<form class="post_review_form">
-        <div class="review-background"></div>
-        <div class="review-window">
-          <div class="review-editer__header"> レビュー </div>
-          <div class="review-editer__score clearfix">
-            <div class="review-editer__score-rate"> - </div>
-            <input class="review-editer__score-slider" type="range" name="review[score]" min="9" max="9" step="1">
-          </div>
-          <div class="review-editer__input-wrapper">
-            <textarea class="review-editer__review-input review-textarea" placeholder="レビューを入力" name="review[comment]"></textarea>
-          </div>
-          <div class="review-editer__tags-wrapper">
-            <textarea class="review-editer__tags-input review-textarea" placeholder="# タグを入力" name="tags[name]"></textarea>
-          </div>
-          <div class="review-editer__bottons-secondary">
-            <div class="review-editer__status-wrapper">
-              <input class="review-status" type="hidden" value="0" name="review[status]">
-              <a class="review-editer__status-botton gray-botton">
-                <i class="fa fa-warning"></i>
-                <p> ネタバレ </p>
-              </a>
-              <p class="review-editer__secret-note">
-                レビューが映画の内容のネタバレに<br>
-                あたる場合はチェックしてください。
-              </p>
-            </div>
-          </div>
-          <div class="review-editer__bottons-primary">
-            <a class="review-editer__botton--post" href="#"> 投稿 </a>
-          </div>
-        </div>
-      </form>`
-  return reviewHTML;
+  function displayReviewPostForm(ele) {
+    ele.css({"display": "inline"})
+    $(".review-editer__score-slider").attr("max", 50);
   }
 
   // ----- レビューウィンドウの表示 -----
-  $(".post-review-btn").on("click", function() {
-    $(".new_review").css({"display": "inline"})
-    $(".review-editer__score-slider").attr("max", 50);
+  $(".post-review-btn").on("click", function(e) {
+    e.preventDefault();
+    var reviewPostForm = $(".new_review")
+    displayReviewPostForm(reviewPostForm);
+  });
+
+  $(".list-content__mark").on("click", function(e) {
+    e.preventDefault();
+    var reviewPostForm = $(this).parents("a").siblings("form");
+    displayReviewPostForm(reviewPostForm);
   });
 
   // ----- レビューウィンドウを閉じる -----
@@ -74,17 +48,20 @@ $(function() {
     };
   });
 
-  // ----- フォーカスが外れた時、全てのタグに # をつける -----
+  // ----- 全てのタグに # をつける -----
   $(document).on("keydown", ".review-editer__tags-input", function() {
-    var tags = $(".review-editer__tags-input").val().replace(/　/g, " ").split(" ");
-    var tagsWithSharp = tags.map(function(ele) {
-      if(ele.match(/^#+.*/)) {
-        ele = ele.replace(/^#+(.*)/, "$1")
-      }
-      return "#" + ele
-    });
-    var tagsToString = tagsWithSharp.join(" ");
-    $(".review-editer__tags-input").val(tagsToString);
+    // var formCSS = $(this).parents(".new_review").css("display")
+    // if(formCSS == "inline") {
+    //   var tags = $(".review-editer__tags-input").val().replace(/　/g, " ").split(" ");
+    //   var tagsWithSharp = tags.map(function(ele) {
+    //     if(ele.match(/^#+.*/)) {
+    //       ele = ele.replace(/^#+(.*)/, "$1")
+    //     }
+    //     return "#" + ele
+    //   });
+    //   var tagsToString = tagsWithSharp.join(" ");
+    //   $(this).val(tagsToString);
+    // };
   });
 
   // ----- ネタバレ禁止の処理 -----
@@ -97,34 +74,4 @@ $(function() {
       $(this).css({"color": "Dimgray", "background-color": "#eee"});
     };
   });
-
-  // ----- データ送信 -----
-  // $(document).on("click", ".review-editer__botton--post", function(e) {
-  //   e.preventDefault();
-  //   var formData = new FormData($(".post_review_form").get()[0]);
-  //   var currentPageURL = window.location.href
-  //   var moviePageReg1 = /\/movies\/\d+$/;
-  //   var moviePageReg2 = /\/movies\/\d+\?.*/;
-  //   var reviewPageReg1 = /\/movies\/\d+\/reviews\/\d+$/;
-  //   var reviewPageReg2 = /\/movies\/\d+\/reviews\/\d+\?.*/;
-  //   if(currentPageURL.match(moviePageReg1)) {
-  //     var postURL = currentPageURL + "/reviews"
-  //   } else if(currentPageURL.match(reviewPageReg1)) {
-  //     var postURL = currentPageURL.replace(/(\/movies\/\d+\/reviews)\/\d+/, "$1")
-  //   };
-
-  //   $.ajax({
-  //     url: postURL,
-  //     type: "POST",
-  //     data: formData,
-  //     processData: false,
-  //     contentType: false
-  //   })
-  //   .done(function(data) {
-  //     alert("レビューを投稿しました。")
-  //   })
-  //   .fail(function() {
-  //     alert("送信に失敗しました。")
-  //   });
-  // });
 });
