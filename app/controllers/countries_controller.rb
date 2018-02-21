4d7335043a0ef91ee8861d22506e61f6b0ef88c2
Yudai Watanabe <lock0308@gmail.com>
2018-02-21 09:43:31 +0900

    国別検索ページでレビューでのレビュー投稿実装、レート数値を表示

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,11 +1,14 @@
 class CountriesController < ApplicationController
 
+  before_action :build_review_and_tag_model
+
   def index
     @search_country = Country.find(params[:country_id])
     @search_country_movies = @search_country.movies.order('time DESC').page(params[:page]).per(36)
     @popular_movies = @search_country_movies.limit(3)
     @country_index = Country.limit(20)
     @country_index_other = Country.offset(20).map{|country| [country,country.movies,country.movies.count]}
+    set_movies_infos(@search_country_movies)
   end
 
   def others
@@ -13,5 +16,28 @@ class CountriesController < ApplicationController
     movies = @country_index_other.map{ |country,movie,count| movie }.flatten
     @search_country_movies = Kaminari.paginate_array(movies).page(params[:page]).per(36)
     @all_movie_count = @country_index_other.map{ |country,movie,count| count }.sum
+    set_movies_infos(@search_country_movies)
+  end
+
+  private
+
+  def build_review_and_tag_model
+    @review = Review.new
+    @tag = Tag.new
+  end
+
+  def set_movies_infos(movies)
+    @movies_infos_array = []
+    movies.each do |movie|
+      scores_array = []
+      movie.reviews.each do |review|
+        scores_array << review.score unless review.score == nil
+      end
+      if scores_array.length > 0
+        score_ave = (scores_array.sum) / (scores_array.length)
+      end
+      movie_infos = {reviews_count: scores_array.length, score_ave: score_ave}
+      @movies_infos_array << movie_infos
+    end
   end
 end
