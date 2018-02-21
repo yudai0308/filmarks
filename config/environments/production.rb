2129d2fc4ee28ce1f5aacb65ed1bf0ed4b50822b
TAN KAZUKi <buppa.mods@gmail.com>
2018-02-21 21:50:55 +0900

    Delete root and Add root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "/path/to/app/current/Gemfile"
+  end
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
