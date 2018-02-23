0f9cc740b8fdbe74c999c67ca39ade5e10f15baa
根本大輔 <defmono112233@gmail.com>
2018-02-23 17:13:10 +0900

    add_マイページのレビュー表示を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def review
+    if user_signed_in?
+      user = current_user
+      @user_actions = user.reviews.order('created_at DESC')
+      @review = Review.new
+      @tag = Tag.new
+      @comment = Comment.new
+    else
+      redirect_to controller: :toppage, action: :index
+    end
   end
-
+    @review = Review.new
+    @tag = Tag.new
+    @comment = Comment.new
   def clip
   end
 end
